7b1a196fc0faf8c7d08189f1743d90c5470f2673
afrisby <afrisby@f0fe87db-556f-44ae-bc77-2877b33714df>
2008-08-20 06:14:21 +0000

    * Adding YATS (Yet another template system) for Code Igniter. * Added YATS configuration.

--- trunk/system/application/controllers/welcome.php
+++ trunk/system/application/controllers/welcome.php
@@ -1,17 +1,24 @@
 <?php
 
-class Welcome extends Controller {
+class Welcome extends Controller
+{
+    function Welcome()
+    {
+        parent::Controller();
+    }
 
-	function Welcome()
-	{
-		parent::Controller();	
-	}
-	
-	function index()
-	{
-		$this->load->view('welcome_message');
-	}
+    function index()
+    {
+        /* 
+         * Do whatever you want and then...
+         */
+
+        // Set the template valiables
+        $data['whatever'] = "If you can read me YATS The Layout Library is working correctly. Good job!";
+
+        // Build the thing
+        $this->layout->buildPage('welcome/home', $data);
+    }
 }
 
-/* End of file welcome.php */
-/* Location: ./system/application/controllers/welcome.php */
+?>
